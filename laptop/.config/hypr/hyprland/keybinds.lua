8592ceb5eb6e34b2c3f081880107bd6bf5725838
davidheeren <davidmheeren@gmail.com>
2026-07-01 11:00:54 -0500

    fix laptop media keybinds

--- laptop/.config/hypr/hyprland/keybinds.lua
+++ laptop/.config/hypr/hyprland/keybinds.lua
@@ -1,5 +1,5 @@
 
 return function(main_mod, playerctl_cmd)
-    hl.bind(main_mod .. " + XF86AudioRaiseVolume", hl.dsp.exec_cmd(playerctl_cmd .. " play-pause"), { locked = true, description = "Play/Pause media" })
-    hl.bind(main_mod .. " + XF86AudioLowerVolume", hl.dsp.exec_cmd(playerctl_cmd .. " play-pause"), { locked = true, description = "Play/Pause media" })
+    hl.bind(main_mod .. " + XF86AudioRaiseVolume", hl.dsp.exec_cmd(playerctl_cmd .. " next"), { locked = true, description = "Play media next" })
+    hl.bind(main_mod .. " + XF86AudioLowerVolume", hl.dsp.exec_cmd(playerctl_cmd .. " previous"), { locked = true, description = "Play media previous" })
 end
