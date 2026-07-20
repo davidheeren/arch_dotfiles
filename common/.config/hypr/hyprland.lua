a88a0011ed8007717505a2da0314451a51be9c0f
davidheeren <davidmheeren@gmail.com>
2026-07-20 16:09:35 -0500

    clipboard concat

--- common/.config/hypr/hyprland.lua
+++ common/.config/hypr/hyprland.lua
@@ -138,10 +138,11 @@ hl.bind(main_mod .. " + A", hl.dsp.exec_cmd("gtk-launch amplenote"), { descripti
 hl.bind(main_mod .. " + T", hl.dsp.exec_cmd("~/bin/bookmark-open"), { description = "Open bookmark url" })
 hl.bind(main_mod .. " + W", hl.dsp.exec_cmd("~/bin/process-toggle waybar"), { description = "Toggle waybar" })
 hl.bind(main_mod .. " + V", hl.dsp.exec_cmd("~/bin/clip-menu"), { description = "Open clipboard history" })
+hl.bind(main_mod .. " + SHIFT + V", hl.dsp.exec_cmd("~/bin/clip-concat"), { description = "Concat clipboard history" })
 hl.bind(main_mod .. " + SHIFT + W", hl.dsp.exec_cmd("~/bin/wallpaper-set"), { description = "Choose wallpaper" })
 hl.bind(main_mod .. " + SHIFT + C", hl.dsp.exec_cmd("~/bin/colorscheme-set"), { description = "Choose colorscheme" })
-hl.bind(main_mod .. " + SHIFT + V", hl.dsp.window.float({ action = "toggle" }), { description = "Toggle floating window" })
-hl.bind(main_mod .. " + SPACE", hl.dsp.exec_cmd("~/bin/process-toggle wofi -S drun -a -p app"), { description = "Open app" })
+hl.bind(main_mod .. " + SHIFT + F", hl.dsp.window.float({ action = "toggle" }), { description = "Toggle floating window" })
+hl.bind(main_mod .. " + SPACE", hl.dsp.exec_cmd("~/bin/process-toggle wofi -aI -S drun -p app"), { description = "Open app" })
 hl.bind(main_mod .. " + SHIFT + P", hl.dsp.window.pin(), { description = "Toggle pin window across workspaces" })
 hl.bind(main_mod .. " + SHIFT + T", hl.dsp.window.set_prop({ prop = "opaque", value = "toggle", window = "active" }),
     { description = "Toggle active window transparency" })
