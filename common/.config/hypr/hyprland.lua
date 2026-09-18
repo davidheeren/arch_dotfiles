e19a5eec19b4997554e88936075e2bd616aac408
davidheeren <davidmheeren@gmail.com>
2026-09-18 17:26:51 -0500

    click finger middle mouse

--- common/.config/hypr/hyprland.lua
+++ common/.config/hypr/hyprland.lua
@@ -120,8 +120,8 @@ hl.config({
 
         touchpad           = {
             natural_scroll = true,
-            scroll_factor = 0.2
-
+            scroll_factor = 0.2,
+            clickfinger_behavior = true
         },
     },
 })
